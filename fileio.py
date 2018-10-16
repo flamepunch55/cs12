5a42ec6a77f33779a91e5f2cd8443afde8b52662
flamepunch55 <43067688+flamepunch55@users.noreply.github.com>
2018-10-16 10:34:49 -0400

    Update

--- fileio.py
+++ fileio.py
@@ -1,1 +1,8 @@
+fobject = open("important.txt", "w")
+fobject.write("go \ngo \ngo \nstop")
+fobject.close()
 
+#fobject = open("important.txt","r")
+#print(fobject.read(5))
+#print(fobject.read())
+#fobject.close()
